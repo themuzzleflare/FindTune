6e1d101a3a78c7ade5532e7e808c9dab4f0fb3a7
Paul Tavitian <paul.tavitian@icloud.com>
2020-11-22 18:15:44 +1100

    Initial Commit

---
+++ FindTune/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindTune
+//
+//  Created by Paul Tavitian on 22/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindTune/FindTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTuneApp.swift
+//  FindTune
+//
+//  Created by Paul Tavitian on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FindTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
